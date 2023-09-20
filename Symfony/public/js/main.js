6cc334bdab7ad92842a4ec9948dd0fc5978ca66b
TSAELENS <saelens.theo@gmail.com>
2023-09-20 15:23:11 +0200

    Formulaire patient

--- Symfony/public/js/main.js
+++ Symfony/public/js/main.js
@@ -1,18 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     const searchForm = document.getElementById('search-form');
     const searchInput = document.querySelector('.search-input');
-
-    searchInput.addEventListener('keydown', function (e) {
-        if (e.key === 'Enter') {
-            e.preventDefault(); // Empêche le comportement par défaut de la touche Entrée
-            searchForm.submit(); // Soumet le formulaire lorsque la touche Entrée est pressée
-        }
-    });
-
     const searchIconLink = document.getElementById('search-icon-link');
 
-    searchIconLink.addEventListener('click', function (e) {
-        e.preventDefault(); // Empêche le lien de naviguer vers une nouvelle page
-        searchForm.submit(); // Soumet le formulaire lorsque l'icône est cliquée
-    });
+    // Vérifiez si searchForm et searchInput existent avant d'ajouter les écouteurs d'événements
+    if (searchForm && searchInput) {
+        searchInput.addEventListener('keydown', function (e) {
+            if (e.key === 'Enter') {
+                e.preventDefault(); // Empêche le comportement par défaut de la touche Entrée
+                searchForm.submit(); // Soumet le formulaire lorsque la touche Entrée est pressée
+            }
+        });
+    }
+
+    // Vérifiez si searchIconLink existe avant d'ajouter l'écouteur d'événements
+    if (searchIconLink && searchForm) {
+        searchIconLink.addEventListener('click', function (e) {
+            e.preventDefault(); // Empêche le lien de naviguer vers une nouvelle page
+            searchForm.submit(); // Soumet le formulaire lorsque l'icône est cliquée
+        });
+    }
 });
